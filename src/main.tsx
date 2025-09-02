d0cce0095377d14edb32fa2d0de06e706f7f7cb0
Elgiz Mamedov <forworkandlesson1123@gmail.com>
2025-09-02 10:26:40 +0100

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ContextProviderThemeAndLang } from './contextReducer/ThemeAndLang.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from "react-router-dom";
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <ContextProviderThemeAndLang>
     <App />
     </ContextProviderThemeAndLang>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
